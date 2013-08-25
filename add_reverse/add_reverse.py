f1333bbb70db0813405af357e6291ba8d0225d8f
Shadaez <JustinTymeWheeler@Gmail.com>
2013-08-25 16:13:55 -0700

    another sample

---
+++ add_reverse/add_reverse.py
@@ -0,0 +1,48 @@
+"""Challenge Description:
+
+Credits: Programming Challenges by Steven S. Skiena and Miguel A. Revilla 
+
+The problem is as follows: choose a number, reverse its digits and add it to the original. If the sum is not a palindrome (which means, it is not the same number from left to right and right to left), repeat this procedure. E.g.
+
+195 (initial number) + 591 (reverse of initial number) = 786
+
+786 + 687 = 1473
+
+1473 + 3741 = 5214
+
+5214 + 4125 = 9339 (palindrome)
+In this particular case the palindrome 9339 appeared after the 4th addition. This method leads to palindromes in a few step for almost all of the integers. But there are interesting exceptions. 196 is the first number for which no palindrome has been found. It is not proven though, that there is no such a palindrome.
+
+Input sample:
+
+Your program should accept as its first argument a path to a filename. Each line in this file is one test case. Each test case will contain an integer n < 10,000. Assume each test case will always have an answer and that it is computable with less than 100 iterations (additions).
+
+Output sample:
+
+For each line of input, generate a line of output which is the number of iterations (additions) to compute the palindrome and the resulting palindrome. (they should be on one line and separated by a single space character). E.g.
+
+4 9339"""
+
+import sys
+
+def is_palindrone(number):
+	number = str(number)
+	length = len(number)
+	half_length = length/2
+	if number[:half_length - (1 - length%2):-1] == number[:half_length]:
+		return True
+	else:
+		return False
+
+def recursive(number, i):
+	if is_palindrone(int(number)):
+			return number, i
+	else:
+		return recursive(int(number) + int(str(number)[::-1]), i+1)
+
+text_file = open(sys.argv[1], 'r')
+
+for line in text_file:
+	results = recursive(int(line), 0)
+	print str(results[1]) + ' ' + str(results[0])
+	
