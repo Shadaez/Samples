9b9b043cf737af1654e152945629a39fc27d51b1
Shadaez <JustinTymeWheeler@Gmail.com>
2013-08-25 16:48:20 -0700

    added challenge description

--- remove_chars/remove_chars.py
+++ remove_chars/remove_chars.py
@@ -1,4 +1,24 @@
-import sys, math
+"""Remove Characters  Share on LinkedIn
+
+Challenge Description:
+
+Write a program to remove specific characters from a string.
+
+Input sample:
+
+The first argument will be a text file containing an input string followed by a comma and then the characters that need to be scrubbed. e.g. 
+
+how are you, abc
+hello world, def
+Output sample:
+
+Print to stdout, the scrubbed strings, one per line. Trim out any leading/trailing whitespaces if they occur. 
+e.g.
+
+how re you
+hllo worl
+"""
+import sys
 
 text_file = open(sys.argv[1], 'r')
 
